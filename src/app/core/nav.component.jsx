4d20ca89fb4040f7a59efa0ce9aeb5ef1d4540d1
Xavier Reyes <xareyesochoa@gmail.com>
2016-11-14 09:54:28 -0800

    completed login and signup forms

--- src/app/core/nav.component.jsx
+++ src/app/core/nav.component.jsx
@@ -2,6 +2,29 @@ import { Link } from 'react-router';
 
 export class Nav extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { username: '', error: '' };
+
+        this.signOut = this.signOut.bind(this);
+    }
+
+    componentWillMount() {
+        //check if user is logged in
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                let username = user.displayName;
+                this.setState({ username: username });
+            }
+        });
+    }
+
+    signOut() {
+        firebase.auth().signOut()
+            .then(() => { this.setState({ username: '' }); })
+            .catch((err) => { this.setState({ error: err.message }); });
+    }
+
     render() {
         return (
             <nav>
@@ -12,6 +35,9 @@ export class Nav extends React.Component {
                 <Link to="/checkout">Checkout</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
+                { this.state.username ? <button onClick={ this.signOut }>Sign Out</button> : null }
+                <p>{ this.state.username }</p>
+                <p>{ this.state.error }</p>
             </nav>
         );
     }
--- src/app/signup.component.jsx
+++ src/app/signup.component.jsx
@@ -1,8 +1,80 @@
 export class Signup extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { username: '', email: '', password: '', error: '' };
+
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onUsernameChange(event) {
+        this.setState({ username: event.target.value });
+    }
+
+    onEmailChange(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    onPasswordChange(event) {
+        this.setState({ password: event.target.value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.signUp(this.state.username, this.state.email, this.state.password);
+    }
+
+
+    signUp(username, email, password) {
+
+        if (!username) {
+            this.setState({ error: 'Please enter a username.' });
+            return;
+        }
+        if (!email) {
+            this.setState({ error: 'Please enter an email address.' });
+            return;
+        }
+        if (!password) {
+            this.setState({ error: 'Please enter a password.' });
+            return;
+        }
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(() => {
+                //save diplayname
+                let user = firebase.auth().currentUser;
+                user.updateProfile({ displayName: username })
+                    .catch(this.handleErrors.bind(this));
+
+            })
+            .catch(this.handleErrors.bind(this));
+    }
+
+    handleErrors(err) {
+        let errorCode = err.code;
+        let errorMessage = err.message;
+        this.setState({ error: errorMessage });
+    }
+
+
     render() {
         return (
-            <h1>Signup</h1>
+            <div>
+            	<h1>Signup</h1>
+            	<form onSubmit={this.onSubmit}>
+            		<label>Username:</label>
+			        <input type="text" value={this.state.username} onChange={this.onUsernameChange} />
+			        <label>Email:</label>
+			        <input type="text" value={this.state.email} onChange={this.onEmailChange} />
+			        <label>Password:</label>
+			        <input type="text" value={this.state.password} onChange={this.onPasswordChange} />
+			        <input type="submit" value="Submit" />
+			    </form>
+			    <p>{this.state.error}</p>
+			</div>
         );
     }
 }
