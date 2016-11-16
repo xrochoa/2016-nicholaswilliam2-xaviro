fccbfb9acae469dba606c7423ba9fd48165f18c4
Xavier Reyes <xareyesochoa@gmail.com>
2016-11-16 00:14:28 -0800

    added some transitions and sticky footer

--- src/app/core/footer.component.jsx
+++ src/app/core/footer.component.jsx
@@ -1,8 +1,19 @@
 export class Footer extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { animate: '' };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ animate: ' animate' });
+        }, 300)
+    }
+
     render() {
         return (
-            <footer id="site-footer">
+            <footer id="site-footer" className={ this.state.animate }>
                 <p>Nicholas William 2016 { String.fromCharCode(169) } All rights reserved</p>
             </footer>
         );
--- src/app/shirts/shirt.component.jsx
+++ src/app/shirts/shirt.component.jsx
@@ -2,25 +2,16 @@ import { Link } from 'react-router';
 
 export class Shirt extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { url: '' };
-    // }
+    constructor(props) {
+        super(props);
+        this.state = { animate: '' };
+    }
 
-
-    //componentWillMount() {
-    //logic to retrieve using firebase storage (unnecessary for this app, to slow and only read is needed, moving it to database instead)
-    // let storageRef = firebase.storage().ref();
-    // storageRef.child(`${ this.formatedImageName() }.png`).getDownloadURL().then((url) => {
-    //     this.setState({ url: url });
-    // }).catch(function(err) {
-    //     console.log('There was a problen getting the shirt image: ', err.message)
-    // });
-    //}
-
-    // formatedImageName() {
-    //     return this.props.shirtData.name.toLowerCase().split(' ').join('_');
-    // }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ animate: ' animate' });
+        }, 300)
+    }
 
     randomHex() {
         return { backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16) };
@@ -28,7 +19,7 @@ export class Shirt extends React.Component {
 
     render() {
         return (
-            <div className="shirt-wrapper">
+            <div className={ 'shirt-wrapper' + this.state.animate }>
                 <h2>{this.props.children}</h2>
                 <Link to={ `/shirts/${ this.props.shirtData.code }`}>
                     <img className="shirt" src="./assets/img/shirt.svg" style={ this.randomHex() }/>
